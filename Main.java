1836721177901523af0e781ecc5eb948e0fc061b
TOLGA MURATDAGI <104718071+muratdagitolga@users.noreply.github.com>
2022-06-04 09:50:01 +0300

    hatalı metod commit. variables changed pipeline has changed first variant has changed

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
   public static void main(String[] args) {
     
     System.out.println("Enter two numbers");
-    int first = 70;
+    int first = 100;
     int second = 80;
     
     System.out.println(first + " " + second);
