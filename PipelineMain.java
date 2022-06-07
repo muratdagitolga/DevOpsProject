1d95c367d04b6a2191f879893cf212e3618fbaa0
TOLGA MURATDAGI <104718071+muratdagitolga@users.noreply.github.com>
2022-06-07 20:00:50 +0300

    hatalı kod gönderildi.

--- PipelineMain.java
+++ PipelineMain.java
@@ -1,19 +1,40 @@
 class PipelineMain {
 
-  public static void main(String[] args) {
-    
-    System.out.println("Enter two numbers");
-    int first = 20;
-    int second = 20;
-    
-    System.out.println(first + " " + second);
+    public static void main(String[] args) {
 
-    // add two numbers
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
-  }
 
-  public int add(int first, int second) {
-    return first + second;
-  }
+        // success code
+        System.out.println("Enter two numbers");
+        int first = 10;
+        int second = 20;
+
+        // add two numbers
+        int summary = add(first, second);
+
+        System.out.println(first + " " + second);
+        System.out.println("The sum is: " + summary);
+
+
+/*
+        // success code
+        System.out.println("Enter two numbers");
+        double first = 10.0;
+        double second = 20.0;
+
+        // add two numbers
+        double summary = add(first, second);
+
+        System.out.println(first + " " + second);
+        System.out.println("The sum is: " + summary);
+         */
+
+    }
+
+    public static int add(int first, int second) {
+        return first + second;
+    }
+
+    public static long add(long first, long second) {
+        return first + second;
+    }
 }
