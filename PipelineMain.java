2b90bd818c01e6e44fc2a587fcdaba9b72724c03
TOLGA MURATDAGI <104718071+muratdagitolga@users.noreply.github.com>
2022-06-07 18:55:22 +0300

    initial commit.

--- PipelineMain.java
+++ PipelineMain.java
@@ -1,19 +1,26 @@
 class PipelineMain {
 
-  public static void main(String[] args) {
-    
-    System.out.println("Enter two numbers");
-    int first = 20;
-    int second = 20;
-    
-    System.out.println(first + " " + second);
+    public static void main(String[] args) {
 
-    // add two numbers
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
-  }
+        System.out.println("Enter two numbers");
+        int first = 20;
+        int second = 20;
 
-  public int add(int first, int second) {
-    return first + second;
-  }
+        // add two numbers
+        int summary = add(first, second);
+        System.out.println(first + " " + second);
+        System.out.println("The sum is: " + summary);
+    }
+
+    public static int add(int first, int second) {
+        return first + second;
+    }
+
+    public static long add(long first, long second) {
+        return first + second;
+    }
+
+    public static double add(double first, double second) {
+        return first + second;
+    }
 }
